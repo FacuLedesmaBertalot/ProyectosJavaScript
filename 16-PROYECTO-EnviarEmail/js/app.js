bb0d38841e019068b7744c44cdb9775a32fd3809
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-10 14:19:18 -0300

    Iniciado Proyecto de simulación de Emails

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Seleccionar los Elementos de la Interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    
+
+});
