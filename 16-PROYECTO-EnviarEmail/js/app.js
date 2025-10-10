f9ca3327669b7351bde80b4772d1548621c0cbb7
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-10 19:02:00 -0300

    Oculta alertas si pasa la validación

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleccionar los Elementos de la Interfaz
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
-    // Seleccionar los Elementos de la Interfaz
-    const inputEmail = document.querySelector('#email');
-    const inputAsunto = document.querySelector('#asunto');
-    const inputMensaje = document.querySelector('#mensaje');
-    const formulario = document.querySelector('#formulario');
-    
-    // Asignar eventos
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+  // Asignar eventos
+  inputEmail.addEventListener("blur", validar);
+  inputAsunto.addEventListener("blur", validar);
+  inputMensaje.addEventListener("blur", validar);
 
-
-    function validar(e) {
-        
-        if (e.target.value.trim() === '') {
-            mostrarAlerta(`El Campo ${e.target.id} es Obligatorio`, e.target.parentElement);
-        } else {
-            console.log('Hay algo');
-        }
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El Campo ${e.target.id} es Obligatorio`,
+        e.target.parentElement
+      );
+      return;
     }
+    limpiarAlerta(e.target.parentElement);
+  }
 
-    function mostrarAlerta(mensaje, referencia) {
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
 
-        // Comprueba si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        if (alerta) {
-            alerta.remove();
-        }
+    // Generar un Alerta en HTML
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
 
+    // Inyectar el error al formulario
+    referencia.appendChild(error);
+  }
 
-
-        // Generar un Alerta en HTML
-        const error = document.createElement('P');
-        error.textContent = mensaje;
-        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
-
-        // Inyectar el error al formulario
-        referencia.appendChild(error);
+  function limpiarAlerta(referencia) {
+    // Comprueba si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
     }
-
+  }
 });
