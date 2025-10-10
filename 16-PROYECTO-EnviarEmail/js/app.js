62beba8e74f8196a34af7800effbc5702791be28
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-10 20:14:09 -0300

    Validar emal con una expresión regular

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -18,6 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
       );
       return;
     }
+
+    if (e.target.id === 'email' && !validarEmail(e.target.value)) {
+        mostrarAlerta('El Email no es Válido', e.target.parentElement);
+        return;
+    }
+
     limpiarAlerta(e.target.parentElement);
   }
 
@@ -40,4 +46,12 @@ document.addEventListener("DOMContentLoaded", function () {
       alerta.remove();
     }
   }
+
+  function validarEmail(email) {
+    const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const resultado = regex.test(email);
+    return resultado;
+
+  }
+
 });
