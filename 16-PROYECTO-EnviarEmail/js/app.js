54183ca0eb3fbfe5743b31933715fb524e9432fe
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-11 20:53:11 -0300

    Resetear el Formulario

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -6,18 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
     mensaje: ''
   }
 
-
-
   // Seleccionar los Elementos de la Interfaz
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
   const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+  const btnReset = document.querySelector('#formulario button[type="reset"]');
 
   // Asignar eventos
   inputEmail.addEventListener("input", validar);
   inputAsunto.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
+
+  btnReset.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Reiniciar el objeto
+    email.email = '';
+    email.asunto = '';
+    email.mensaje = '';
+
+    formulario.reset();
+    comprobarEmail();
+  })
 
   function validar(e) {
     if (e.target.value.trim() === "") {
