c68ad22b189f89cb632e543e349be3ca0f250b29
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-12 20:26:28 -0300

    Generando un select de años

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,12 +1,20 @@
 // Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
 
+const max = new Date().getFullYear();
+const min = max - 10;
 
 
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
+    mostrarAutos(); // muestra los autos al cargar
+
+
+    // Llena las opciones de años
+    llenarSelect();
+
 })
 
 
@@ -26,4 +34,15 @@ function mostrarAutos() {
         // Insertar en el HTML
         resultado.appendChild(autoHTML);
     })
+}
+
+// Genera los años del select
+function llenarSelect() {
+
+    for (let i = max ; i >= min ; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones del año al select
+    }
 }
