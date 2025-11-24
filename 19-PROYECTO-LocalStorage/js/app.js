675c6dfea720da6ba341630b492b2b41f110a7a2
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-11-24 15:40:15 -0300

    Primer commit

---
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -0,0 +1,21 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+
+// Event Listeners
+eventListeners();
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+
+    
+}
