38112bb0cf02b4b598c87b093fa09e52b8531634
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-11-24 16:15:51 -0300

    Añadiendo Validaciones

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -12,10 +12,34 @@ function eventListeners() {
 
 
 
-
 // Funciones
 function agregarTweet(e) {
     e.preventDefault();
 
-    
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validación
+    if (tweet === '') {
+        mostrarError('Un Mensaje No Puede ir Vacío');
+
+        return; // Evita que se ejecuten más líneas de código
+    }
+
+}
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el Contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta después de 3s
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
