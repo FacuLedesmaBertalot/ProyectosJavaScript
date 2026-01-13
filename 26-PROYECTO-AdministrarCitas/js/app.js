ec08cb4f61aa128820f8f219bf06c2bd47bc5f8d
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-13 15:34:28 -0300

    Creando funcion re-utilizable

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -1,20 +1,9 @@
 // Selectores
-
 const pacienteInput = document.querySelector('#paciente');
 const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
-
-
-// Objeto de Cita
-const citaObj = {
-    paciente: '',
-    propietario: '',
-    email: '',
-    fecha: '',
-    sintomas: ''
-}
 
 
 // Eventos
@@ -25,6 +14,14 @@ fechaInput.addEventListener('change', datosCita);
 sintomasInput.addEventListener('change', datosCita);
 
 
+// Objeto de Cita
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: ''
+}
 
 
 function datosCita(e) {
