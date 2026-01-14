e02ab6cb690eba6c391bf8a6534e052da8cb55f8
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-14 10:52:05 -0300

    Prevent duplicate alerts

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -58,6 +58,10 @@ class Notificacion {
         const alerta = document.createElement('DIV');
         alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
 
+        // Eliminar alertas duplicadas
+        const alertaPrevia = document.querySelector('.alert');
+        alertaPrevia?.remove();
+
         // Si es de tipo error, agrega una clase
         this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alert.classList.add('bg-green-500');
 
@@ -67,5 +71,10 @@ class Notificacion {
 
         // Insertar en el DOM
         formulario.parentElement.insertBefore(alerta, formulario)
+
+        // Quitar después de 3 segundos
+        setTimeout(() => {
+            alerta.remove();    
+        }, 3000);
     }
 }
