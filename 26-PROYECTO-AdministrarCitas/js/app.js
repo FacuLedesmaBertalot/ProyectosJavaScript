daba6e1da88525150d5d3d2ebb9f2ec1502e7de0
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-14 12:02:54 -0300

    First steps edit button

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -9,6 +9,7 @@ const formulario = document.querySelector("#formulario-cita");
 
 const contenedorCitas = document.querySelector("#citas");
 
+
 // Eventos
 pacienteInput.addEventListener("change", datosCita);
 propietarioInput.addEventListener("change", datosCita);
@@ -160,10 +161,14 @@ class AdminCitas {
         "rounded-lg",
         "flex",
         "items-center",
-        "gap-2"
+        "gap-2",
+        "btn-editar"
       );
       btnEditar.innerHTML =
         'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>';
+        btnEditar.onclick = () => {
+            alert('click');
+        }
 
       const btnEliminar = document.createElement("button");
       btnEliminar.classList.add(
