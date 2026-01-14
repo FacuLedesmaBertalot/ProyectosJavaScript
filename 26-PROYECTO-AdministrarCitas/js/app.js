dce2f779321b04ff6efa2f8c24c93f2c66696ffd
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-14 12:18:37 -0300

    Add information to objCita

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -166,9 +166,8 @@ class AdminCitas {
       );
       btnEditar.innerHTML =
         'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>';
-        btnEditar.onclick = () => {
-            alert('click');
-        }
+        const clone = structuredClone(cita);
+        btnEditar.onclick = () => cargarEdicion(clone);
 
       const btnEliminar = document.createElement("button");
       btnEliminar.classList.add(
@@ -240,4 +239,9 @@ function reiniciarObjetoCita() {
     fecha: "",
     sintomas: "",
   });
+}
+
+
+function cargarEdicion() {
+
 }
