702dc48df0444663017d28d6e1fbbf584b72c4ca
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-15 12:47:58 -0300

    Learning Symbols

---
+++ 27-SetsMapsSymbols/js/05-app.js
@@ -0,0 +1,32 @@
+const sym = Symbol('1');
+const sym2 = Symbol('1');
+
+const nombre = Symbol();
+const apellido = Symbol();
+
+const persona = {}
+
+
+// Agregar nombre y apellido como llaves del objeto
+persona[nombre] = 'Facu';
+persona[apellido] = 'Ledesma';
+persona.tipoCliente = 'Premium';
+persona.saldo = 500;
+
+console.log(persona);
+// console.log(persona[nombre]);
+
+// Las propiedades que utilizan un symbol no son iterables
+for (let i in persona) {
+    console.log(i);
+}
+
+// Definir una descripción del symbol
+const nombreCliente = Symbol('Nombre del Cliente');
+const cliente = {};
+
+cliente[nombreCliente] = 'Pedro';
+
+console.log(cliente);
+console.log(cliente[nombreCliente]);
+console.log(nombreCliente);
