acd3481bcc67ed1fd53dccb7f3f2239822e3a703
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-15 10:53:59 -0300

    Delete dates

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -91,10 +91,21 @@ class AdminCitas {
     this.mostrar();
   }
 
+  eliminar(id) {
+    this.citas = this.citas.filter(cita => cita.id !== id);
+    this.mostrar();
+  }
+
   mostrar() {
     // Limpiar el HTML
     while (contenedorCitas.firstChild) {
       contenedorCitas.removeChild(contenedorCitas.firstChild);
+    }
+
+    // Si hay citas
+    if (this.citas.length === 0) {
+      contenedorCitas.innerHTML = '<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>';
+      return;
     }
 
     // Generando las Citas
@@ -193,6 +204,8 @@ class AdminCitas {
       );
       btnEliminar.innerHTML =
         'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
+
+      btnEliminar.onclick = () => this.eliminar(cita.id);
 
       const contenedorBotones = document.createElement("DIV");
       contenedorBotones.classList.add("flex", "justify-between", "mt-10");
