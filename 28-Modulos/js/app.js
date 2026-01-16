1c70c7f307a341e573cc704c96b566b8f9485086
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-16 10:31:26 -0300

    Export and Import a class

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,7 +1,11 @@
-import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo } from "./cliente";
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente";
 
 console.log(nombreCliente);
 console.log(ahorro);
 console.log(mostrarInformacion(nombreCliente, ahorro));
 
 tieneSaldo(ahorro);
+
+const cliente = new Cliente(nombreCliente, ahorro);
+
+console.log(cliente.mostrarInformacion());
