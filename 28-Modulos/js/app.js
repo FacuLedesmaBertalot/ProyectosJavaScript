3422a72b0792cd14e090defd03e4daf67cb22d18
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-16 10:52:23 -0300

    Extend imported class

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,5 @@
 import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente";
+import { Empresa } from './empresa.js';
 
 console.log(nombreCliente);
 console.log(ahorro);
@@ -9,3 +10,7 @@ tieneSaldo(ahorro);
 const cliente = new Cliente(nombreCliente, ahorro);
 
 console.log(cliente.mostrarInformacion());
+
+// Importar empresa
+const empresa = new Empresa('Código con Juan', 100, 'Aprendizaje en Línea');
+console.log(empresa.mostrarInformacion());
