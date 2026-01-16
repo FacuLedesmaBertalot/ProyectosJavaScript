95291b03e12428c0f2ca00bb640f2264206f2a70
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-16 10:26:25 -0300

    Export and Import functions

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,7 @@
-import { nombreCliente, ahorro } from "./cliente";
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo } from "./cliente";
 
 console.log(nombreCliente);
 console.log(ahorro);
+console.log(mostrarInformacion(nombreCliente, ahorro));
+
+tieneSaldo(ahorro);
