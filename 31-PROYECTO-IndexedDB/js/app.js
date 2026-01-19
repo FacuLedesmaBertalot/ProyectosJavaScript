c24ade3d9d11a3d44f7a1e496b7889902eb56295
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-19 09:47:02 -0300

    Add dates to a DB

--- 31-PROYECTO-IndexedDB/js/app.js
+++ 31-PROYECTO-IndexedDB/js/app.js
@@ -202,7 +202,7 @@ function nuevaCita(e) {
         editando = false;
 
     } else {
-        // Nuevo Registrando
+        // Nuevo Registro
 
         // Generar un ID único
         citaObj.id = Date.now();
@@ -210,8 +210,23 @@ function nuevaCita(e) {
         // Añade la nueva cita
         administrarCitas.agregarCita({...citaObj});
 
-        // Mostrar mensaje de que todo esta bien...
-        ui.imprimirAlerta('Se agregó correctamente')
+        // Insertar Registro en IndexDB
+        const transaction = DB.transaction(['citas'], 'readwrite');
+
+        // Habilitar el objectstore
+        const objectStore = transaction.objectStore('citas');
+
+        // Insertar en la DB
+        objectStore.add(citaObj);
+
+        transaction.oncomplete = function() {
+            console.log('Cita Agregada');
+
+            // Mostrar mensaje de que todo esta bien...
+            ui.imprimirAlerta('Se agregó correctamente')
+        }
+
+
     }
 
 
