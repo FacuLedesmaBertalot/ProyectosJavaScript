5a43169ed5c988374f1d6d36d8513793ea7ec9f0
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-02 15:12:07 -0300

    Validando el form

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
-const resultado = document.querySelector('.#resultado');
-const formulario = document.querySelector('.#formulario');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
 
 window.addEventListener('load', () => {
     formulario.addEventListener('submit', buscarClima);
@@ -10,10 +10,43 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
-    console.log('Buscando el clima');
-
     // Validar 
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+
+    if (ciudad === '' || pais === '') {
+        // Hubo un error
+        mostrarError('Ambos Campos son Obligatorios');
+        return;
+    }
+
 
     // Consultar la API
-    
+
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if (!alerta) {
+        // Crear una alerta 
+        const alerta = document.createElement('DIV');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+
+        container.appendChild(alerta);
+
+        // Se elimine la alerta despues de 3 segundos
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+        }
+
 }
