9caf5799498f73acd56c0262e0a7575150418b98
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-04 12:06:48 -0300

    Obtener los platos de la categoria seleccionada

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,6 +1,7 @@
 function iniciarApp() {
 
     const selectCategorias = document.querySelector('#categorias');
+    selectCategorias.addEventListener('change', seleccionarCategoria);
 
     obtenerCategorias();
 
@@ -22,6 +23,16 @@ function iniciarApp() {
             selectCategorias.appendChild(option);
             
         })
+    }
+
+
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => console.log(resultado));
     }
     
 }
