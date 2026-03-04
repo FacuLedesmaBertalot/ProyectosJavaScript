8da736ff939ca87415aa3f09e984aaad906dc842
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-04 16:02:46 -0300

    Comprobar si una receta ya existe en localStorage

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -146,6 +146,9 @@ function iniciarApp() {
         
         // localStorage
         btnFavorito.onclick = function() {
+            if (existeStorage(idMeal)) {
+                return;
+            }
             agregarFavorito({
                 id: idMeal,
                 titulo: strMeal,
@@ -172,6 +175,13 @@ function iniciarApp() {
     function agregarFavorito(receta) {
         const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
         localStorage.setItem('favoritos', JSON.stringify([...favoritos, receta]));
+    }
+
+
+    function existeStorage(id) {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        return favoritos.some(favorito => favorito.id === id);
+
     }
 
 
