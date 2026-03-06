1b9b147dced50d118bdefce929b9bd15b595d688
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-06 12:28:39 -0300

    Primeros pasos con el resumen de pedido

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -89,7 +89,7 @@ function mostrarPlatillos(platillos) {
         precio.textContent = `$${platillo.precio}`;
 
         const categoria = document.createElement('DIV');
-        categoria.classList.add('cold-md-3');
+        categoria.classList.add('colf-md-3');
         categoria.textContent = categorias[platillo.categoria];
 
         const inputCantidad = document.createElement('INPUT');
@@ -145,6 +145,43 @@ function agregarPlatillo(producto) {
     } else {
         // Eliminar elementos cuando la cantidad es 0
         const resultado = pedido.filter( articulo => articulo.id !== producto.id );
-        
+        cliente.pedido = [...resultado];
     }
+
+    // Mostrar el resumen
+    actualizarResumen();
+}
+
+
+function actualizarResumen() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const resumen = document.createElement('DIV');
+    resumen.classList.add('col-md-6');
+
+    // Informacion de la mesa
+    const mesa = document.createElement('P');
+    mesa.textContent = 'Mesa: ';
+    mesa.classList.add('fw-bold');
+
+    const mesaSpan = document.createElement('SPAN');
+    mesaSpan.textContent = cliente.mesa;
+    mesaSpan.classList.add('fw-normal');
+
+    // Informacion de la hora
+    const hora = document.createElement('P');
+    hora.textContent = 'Hora: ';
+    hora.classList.add('fw-bold');
+
+    const horaSpan = document.createElement('SPAN');
+    horaSpan.textContent = cliente.hora;
+    horaSpan.classList.add('fw-normal');
+
+    // Agrega a los elementos padre
+    mesa.appendChild(mesaSpan);
+    hora.appendChild(horaSpan);
+
+    // Agregar al contenido
+    contenido.appendChild(mesa);
+    contenido.appendChild(hora);
 }
