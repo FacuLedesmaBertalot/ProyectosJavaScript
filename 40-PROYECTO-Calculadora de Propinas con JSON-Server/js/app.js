9755c4f2648bd90860a46cc7f44ee8406e665331
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-06 13:19:53 -0300

    Eliminar productos desde el resumen

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -231,6 +231,16 @@ function actualizarResumen() {
         subtotalValor.classList.add('fw-normal');
         subtotalValor.textContent = calcularSubtotal(precio, cantidad);
 
+        // Boton para eliminar
+        const btnEliminar = document.createElement('BUTTON');
+        btnEliminar.classList.add('btn', 'btn-danger');
+        btnEliminar.textContent = 'Eliminar del Pedido';
+
+        // Funcion para eliminar del pedido
+        btnEliminar.onclick = function() {
+            eliminarProducto(id);
+        }
+
         // Agregar Valores a sus contenedores
         cantidadEl.appendChild(cantidadValor);
         precioEl.appendChild(precioValor);
@@ -241,6 +251,7 @@ function actualizarResumen() {
         lista.appendChild(cantidadEl);
         lista.appendChild(precioEl);
         lista.appendChild(subtotalEl);
+        lista.appendChild(btnEliminar);
 
         // Agregar lista al grupo principal
         grupo.appendChild(lista);
@@ -267,4 +278,17 @@ function limpiarHTML() {
 
 function calcularSubtotal(precio, cantidad) {
     return `$ ${precio * cantidad}`;
+}
+
+
+function eliminarProducto(id) {
+    const { pedido } = cliente;
+    const resultado = pedido.filter( articulo => articulo.id !== id );
+    cliente.pedido = [...resultado]; 
+
+    // Limpiar HTML
+    limpiarHTML();
+
+    // Mostrar el resumen
+    actualizarResumen();
 }
