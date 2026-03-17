bcc9bee1ed5189f0c4021c5a7bb87fcf99d14789
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-17 11:01:10 -0300

    Closure, forma de acceder a un valor desde el exterior

---
+++ 45-FunctionalJS/js/08-app.js
@@ -0,0 +1,12 @@
+const obtenerCliente = () => {
+    const nombre = 'Juan';
+
+    function muestraNombre() {
+        console.log(nombre);
+    }
+
+    return muestraNombre;
+}
+
+const cliente = obtenerCliente();
+cliente();
