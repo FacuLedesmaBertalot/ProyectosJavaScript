dbf40ce113e53f6c8bad9eae92c252e8fb8d766d
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-17 10:00:45 -0300

    Guarda cambios del cliente al editar

--- 44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
@@ -1,4 +1,5 @@
-import { obtenerCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js';
+import { obtenerCliente, editarCliente } from './API.js';
 
 (function() {
 
@@ -17,6 +18,10 @@ import { obtenerCliente } from './API.js';
         const cliente = await obtenerCliente(idCliente);
 
         mostrarCliente(cliente);
+
+        // Submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
     });
 
 
@@ -28,5 +33,29 @@ import { obtenerCliente } from './API.js';
         emailInput.value = email;
         telefonoInput.value = telefono;
         idInput.value = id;
+    }
+
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+
+        if (validar(cliente)) {
+            // Mostrar mensaje
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+
+        // Reescribe el codigo
+        editarCliente(cliente);
     }
 })();
