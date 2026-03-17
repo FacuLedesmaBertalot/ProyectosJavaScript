aecf7fb434d51ecf31b8f0a3e3357b94ef6ada82
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-17 11:43:16 -0300

    Hoisting

---
+++ 46-DominandoJavaScript/js/02-app.js
@@ -0,0 +1,9 @@
+obtenerCliente('Juan');
+
+function obtenerCliente(nombre) {
+    console.log(`El nombre del cliente es ${nombre}`);
+}
+
+obtenerCliente2 = function(nombre) {
+    console.log(`El nombre del cliente es ${nombre}`);
+}
