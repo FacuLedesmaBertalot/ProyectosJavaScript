3c20da7f1249562fd795cd67e9c257b431755a38
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-17 11:38:27 -0300

    Scope

---
+++ 46-DominandoJavaScript/js/01-app.js
@@ -0,0 +1,13 @@
+const login = true;
+const cliente = 'Juan';
+
+function clienteLogueado() {
+    console.log(cliente);
+}
+
+function funcion2() {
+    console.log(cliente);
+}
+
+funcion2();
+clienteLogueado();
