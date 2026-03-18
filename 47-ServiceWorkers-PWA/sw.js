57caa2134c725af6fdfdfe631764ad234583ffca
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-18 11:44:20 -0300

    Instalar y Activar un Service Worker

---
+++ 47-ServiceWorkers-PWA/sw.js
@@ -0,0 +1,13 @@
+// Cuando se instala el service worker
+self.addEventListener('install', e => {
+    console.log('Instalando el Service Worker');
+
+    console.log(e);
+});
+
+// Activar el Service Worker
+self.addEventListener('activate', e => {
+    console.log('Service Worker Activado');
+
+    console.log(e);
+})
