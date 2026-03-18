f88ab490423df5ad2d5c098e9f734415bd9e94e5
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-18 11:51:46 -0300

    Agregar soporte Offline

--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -32,4 +32,11 @@ self.addEventListener('activate', e => {
 // Evento fetch para descargar archivos estáticos
 self.addEventListener('fetch', e => {
     console.log('Fetch...', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then(respuestaCache => {
+                return respuestaCache;
+            })
+    )
 });
